4512bfe6a2634273f84cf33834a331749c3273ab
Andrey <Andreypnick@gmail.com>
2023-05-12 21:01:12 +0300

    feat: Map , test style: plugins xml fix: checkStyle.xml :styled:

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -21,9 +21,9 @@ public class FilmControllerTest {
 
     @Test
     public void shouldNotСreateFilmWithWrongRealeaseDate() {
-        Film film = new Film("Star-Wars" , "Galactic war" ,
-                LocalDate.of(1700 , 10 , 10) , 10);
-        Assertions.assertThrows(ValidationException.class , () -> {
+        Film film = new Film("Star-Wars", "Galactic war",
+                LocalDate.of(1700, 10, 10), 10);
+        Assertions.assertThrows(ValidationException.class, () -> {
             filmController.createFilm(film);
         });
     }
@@ -31,43 +31,43 @@ public class FilmControllerTest {
     @Test
     public void shouldNotСreateFilmWithWrongDuration() {
         Film film = new Film("Star-Wars" , "Galactic war" ,
-                LocalDate.of(2000 , 10 , 10) , 0);
-        Assertions.assertThrows(ValidationException.class , () -> {
+                LocalDate.of(2000, 10, 10) , 0);
+        Assertions.assertThrows(ValidationException.class, () -> {
             filmController.createFilm(film);
         });
     }
 
     @Test
     public void shouldNotСreateFilmWithWrongDescription() {
-        Film film = new Film("Star-Wars" , "Galactic war" ,
-                LocalDate.of(2000 , 10 , 10) , 0);
-        Assertions.assertThrows(ValidationException.class , () -> {
+        Film film = new Film("Star-Wars", "Galactic war" ,
+                LocalDate.of(2000, 10, 10) , 0);
+        Assertions.assertThrows(ValidationException.class, () -> {
             filmController.createFilm(film);
         });
     }
 
     @Test
     public void shouldCreateFilm() {
-        Film film = new Film("Star-Wars" , "Galactic war" ,
-                LocalDate.of(2000 , 10 , 10) , 1);
+        Film film = new Film("Star-Wars", "Galactic war" ,
+                LocalDate.of(2000, 10, 10), 1);
         film.setId(1);
         Assertions.assertEquals(film , filmController.createFilm(film));
     }
 
     @Test
     public void shouldNotCreateFilmWithEmptyName() {
-        Film film = new Film("" , "Galactic war" ,
-                LocalDate.of(2000 , 10 , 10) , 12);
-        Assertions.assertThrows(ValidationException.class , () -> {
+        Film film = new Film("", "Galactic war",
+                LocalDate.of(2000, 10 , 10) , 12);
+        Assertions.assertThrows(ValidationException.class, () -> {
             filmController.createFilm(film);
         });
     }
 
     @Test
     public void shouldNotCreateFilmWithNullName() {
-        Film film = new Film(null , "Galactic war" ,
-                LocalDate.of(2000 , 10 , 10) , 12);
-        Assertions.assertThrows(ValidationException.class , () -> {
+        Film film = new Film(null, "Galactic war",
+                LocalDate.of(2000, 10, 10), 12);
+        Assertions.assertThrows(ValidationException.class, () -> {
             filmController.createFilm(film);
         });
     }
