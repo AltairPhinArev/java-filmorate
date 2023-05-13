98e7560e91e3e1523ce2cad4f15edaa7216c9ba5
Andrey <Andreypnick@gmail.com>
2023-05-13 13:49:56 +0300

    style: name of maps , fix: logs level from debug to info

--- src/main/java/ru/yandex/practicum/filmorate/Controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/Controllers/FilmController.java
@@ -6,51 +6,47 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 
 @RestController
 public class FilmController {
     private int filmId = 1;
     private static final Logger log = LogManager.getLogger(Film.class);
-    private final List<Film> films = new ArrayList<>();
-    private final HashMap<Integer, Film> filmMap = new HashMap<>();
+    private final HashMap<Integer, Film> filmById = new HashMap<>();
 
     @GetMapping(value = "/films")
     public Collection<Film> findAll() {
-        log.info("Текущее количество фильмов {}", filmMap.size());
-        return filmMap.values();
+        log.info("Текущее количество фильмов {}", filmById.size());
+        return filmById.values();
     }
 
     @PostMapping(value = "/films")
     public Film createFilm(@RequestBody Film film) {
         validate(film);
         film.setId(filmId++);
-        filmMap.put(film.getId(), film);
+        filmById.put(film.getId(), film);
         log.info("Фильм успшно добавлен");
         return film;
     }
 
     @PutMapping(value = "/films")
     public Film updateUser(@RequestBody Film film) {
-        if (!filmMap.containsKey(film.getId())) {
+        if (!filmById.containsKey(film.getId())) {
             throw new ValidationException();
         } else {
             validate(film);
-            filmMap.put(film.getId(), film);
+            filmById.put(film.getId(), film);
             log.info("Фильм успешно обнавлен с id {}", film.getId());
             return film;
         }
     }
 
-    private Film validate(Film film) {
+    private void validate(Film film) {
         if (film.getName() != null &&
                 !film.getName().isEmpty() &&
                 film.getReleaseDate().isAfter(LocalDate.of(1895, 1, 28)) &&
                 film.getDescription().length() < 200 && film.getDuration() > 0) {
-            return film;
         } else {
             log.error("Не верно укзаны данные фильма");
             throw new ValidationException();
--- src/main/java/ru/yandex/practicum/filmorate/Controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/Controllers/UserController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.Controllers;
+
 import org.springframework.web.bind.annotation.*;
+
 import ru.yandex.practicum.filmorate.Exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -15,43 +17,42 @@ import org.apache.logging.log4j.Logger;
 
         private int userId = 1;
         private static final Logger log = LogManager.getLogger(User.class);
-        private final HashMap<Integer, User> userMap = new HashMap<>();
+        private final HashMap<Integer, User> userById = new HashMap<>();
 
         @GetMapping(value = "/users")
         public Collection<User> findAll() {
-            log.debug("Текущее количество юзеров {}", userMap.size());
-            return userMap.values();
+            log.info("Текущее количество юзеров {}", userById.size());
+            return userById.values();
         }
 
         @PostMapping(value = "/users")
         public User createUser(@RequestBody User user) {
             validate(user);
             user.setId(userId++);
-            userMap.put(userId - 1, user);
+            userById.put(user.getId(), user);
             log.info("Пользователь успшно добавлен");
             return user;
         }
 
         @PutMapping(value = "/users")
         public User updateUser(@RequestBody User user) {
-            if (!userMap.containsKey(user.getId())) {
+            if (!userById.containsKey(user.getId())) {
                 throw new ValidationException();
             } else {
                 validate(user);
-                userMap.put(user.getId(), user);
+                userById.put(user.getId(), user);
                 log.info("Данные пользователя обновлены с id {}", user.getId());
                 return user;
             }
         }
 
-        private User validate(User user) {
+        private void validate(User user) {
             if (user.getEmail() != null && user.getBirthday().isBefore(LocalDate.now()) && user.getLogin() != null &&
                     !user.getLogin().contains(" ") && user.getEmail().contains("@")) {
                     if (user.getName() == null || user.getName().isBlank()) {
                         user.setName(user.getLogin());
                     }
-                    return user;
-                } else {
+            } else {
                     log.error("Не верно укзаны данные Пользователя");
                     throw new ValidationException();
                 }
