4e646f73adb1f85ba4007fdff85cf62a3cd21906
Andrey Panasiuk <andreypnick@gmail.com>
2023-05-25 16:07:26 +0300

    feat exceptions,likes,friends,JUnitTests (Styled Ate-mp 2)

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,8 +4,6 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
