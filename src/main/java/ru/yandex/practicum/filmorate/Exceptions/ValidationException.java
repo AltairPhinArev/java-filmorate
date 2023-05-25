ca5c21646c88d4cc0bcddfccb9ddde9f0a9a9ce4
Andrey Panasiuk <andreypnick@gmail.com>
2023-05-25 16:05:22 +0300

    feat exceptions,likes,friends,JUnitTests (Styled)

--- src/main/java/ru/yandex/practicum/filmorate/Exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exceptions/ValidationException.java
@@ -7,7 +7,7 @@ public class ValidationException extends RuntimeException {
 
     HttpStatus httpStatus;
 
-    public ValidationException(HttpStatus httpStatus ,String message) {
+    public ValidationException(HttpStatus httpStatus, String message) {
         super(message);
         this.httpStatus = httpStatus;
     }
